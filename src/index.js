502cba4eebb78ed4811e2921d50d7e23cd417339
vaishali998 <vaishalikutleria44@gmail.com>
2019-12-08 23:40:01 +0530

    Added 'AddSubscriber' component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import AddSusbscriber from './AddSubscriber';
+import './common/common.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AddSusbscriber />, document.getElementById('root'));
 registerServiceWorker();
